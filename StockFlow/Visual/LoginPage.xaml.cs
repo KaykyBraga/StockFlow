fd4f5e3f801c10d9dbe198964c739386684e635a
unknown <andremoreiraguilherme@gmail.com>
2025-10-16 19:07:58 -0300

    Tela de Login funfando 10%

---
+++ StockFlow/Visual/LoginPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StockFlow.Visual
+{
+    /// <summary>
+    /// Interação lógica para LoginPage.xam
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Hyperlink_Forgot_Click(object sender, RoutedEventArgs e)
+        {
+            // Navega para a RecoveryPage
+            NavigationService.GetNavigationService(this)?.Navigate(new RecoveryPage());
+        }
+    }
+}
