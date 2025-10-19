5110b577b10810c198c463882e48258c4099cb33
Kayky Braga <kayky1133@gmail.com>
2025-10-18 23:02:12 -0300

    Tabelas Faltantes

---
+++ StockFlow/Modelo/Caixa.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockFlow.Modelo
+{
+    public class Caixa
+    {
+        public int CaixaId { get; set; }
+        public int UsuarioAberturaId { get; set; }
+        public DateTime DataHoraAbertura { get; set; }
+        public decimal ValorAbertura { get; set; }
+        public int? UsuarioFechamentoId { get; set; }
+        public DateTime? DataHoraFechamento { get; set; }
+        public decimal? ValorFechamentoCalculado { get; set; }
+        public decimal? ValorFechamentoInformado { get; set; }
+        public decimal? ValorFechamentoCaixa { get; set; }
+        public decimal? Diferenca { get; set; }
+        public string Status { get; set; } // "Aberto" ou "Fechado"
+        public virtual Usuario UsuarioAbertura { get; set; }
+
+        public virtual ICollection<MovimentacaoCaixa> MovimentacaoCaixa { get; set; }
+
+        public Caixa()
+        {
+
+            MovimentacaoCaixa = new HashSet<MovimentacaoCaixa>();
+        }
+    }
+}
---
+++ StockFlow/Modelo/MovimentacaoCaixa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockFlow.Modelo
+{
+    public class MovimentacaoCaixa
+    {
+        public int MovimentacaoCaixaId { get; set; }
+        public int CaixaId { get; set; }
+        public int? VendaId { get; set; }
+        public DateTime DataHora { get; set; }
+        public decimal Valor { get; set; }
+        public string TipoMovimentacao { get; set; } // "Entrada", "Saída", "Abertura", "Sangria"
+        public string MetodoDePagamento { get; set; } // "Dinheiro", "Cartão"
+        public string Descricao { get; set; }
+        public virtual Caixa Caixa { get; set; }
+        public virtual Venda Venda { get; set; }
+    }
+}
